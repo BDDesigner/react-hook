e78d2651f2bbdfed0a3c7082e1b727613bb2a979
BDDesigner <abdulmajed.cse555@gmail.com>
2021-09-26 14:42:56 +0600

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,9 @@
-import React from 'react';
-import ClickCounter from './ClickCounter';
-import Counter from './Counter';
-import HoverCounter from './HoverCounter';
-
 
 function App() {
   return (
-    <div>
-      <Counter render={(count, incrementCount) => <ClickCounter count={count} incrementCount={incrementCount} />} />
-      <Counter render={(count, incrementCount) => <HoverCounter count={count} incrementCount={incrementCount} />} />
-    </div>
+      <div>
+        <h1>Context API</h1>
+      </div>
   );
 }
 
