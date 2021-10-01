74e7a7e13233dd74184d1657cdda9c20a2136d31
BDDesigner <abdulmajed.cse555@gmail.com>
2021-10-01 16:38:32 +0600

    React Form Source Code Poush

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,11 @@
 import React from 'react'
-import DataProvider from '../contexts/DataProvider';
-import Dada from './Dada';
-import Nati from './Nati';
-import Poti from './Poti';
+import Form from './Form'
 
 const App = () => {
 
-  const msg ={
-    dada: {
-      phone: '01761334863',
-      address: 'Rangpur'
-    },
-    nati: '02',
-    poti: '03'
-  }
-
   return (
     <div>
-      <DataProvider.Provider value={msg}>
-        <Dada />
-        <Nati />
-        <Poti />
-      </DataProvider.Provider>
+      <Form />
     </div>
   )
 }
--- src/contexts/DataProvider.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const DataProvider = React.createContext();
-
-export default DataProvider;
